3cc5c4fbdeece03448797a6c6ef4fc6b7e519367
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2023-10-27 16:22:38 +0100

    some stuff

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import json
 import curses
+import os
 
 MUSIC_DIR = "/home/deck/Music"
 CAL_DIR = ".cal_sonic_library"
 
+# Load the list of artists
 with open(f"{MUSIC_DIR}/{CAL_DIR}/meta/artists.json", "r") as file:
     artists_list = json.load(file)
+
+def load_artist_data(artist_name):
+    artist_file = os.path.join(MUSIC_DIR, CAL_DIR, "artists", f"{artist_name}.json")
+    if os.path.isfile(artist_file):
+        with open(artist_file, "r") as file:
+            return json.load(file)
+    else:
+        return []
 
 def main(stdscr):
     curses.curs_set(0)  # Hide the cursor
@@ -39,5 +49,19 @@ def main(stdscr):
             start_row -= 1
         elif key == ord('q'):
             break
+        elif key == 10:  # Enter key
+            selected_artist = artists_list[selected_row + start_row]
+            artist_name = selected_artist["name"]
+            artist_data = load_artist_data(artist_name)
+            
+            # Create a new curses window to display the artist's data
+            artist_data_window = curses.newwin(height, width, 0, 0)
+            artist_data_window.clear()
+            
+            for i, data_item in enumerate(artist_data):
+                artist_data_window.addstr(i, 0, data_item)
+            
+            artist_data_window.refresh()
+            artist_data_window.getch()
 
 curses.wrapper(main)
