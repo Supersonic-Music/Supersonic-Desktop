241d6c590e36c88deb59af17d36f6c3fa0cd7b2a
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2023-11-01 23:03:42 +0000

    Change default server to public instance

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # Specify your Sonic Screwdriver music folder here:
-MUSIC_DIR = "http://localhost:8083/music"
+MUSIC_DIR = "http://atomic123.pythonanywhere.com/music" # Public Server
+# MUSIC_DIR = "http://localhost:8083/music"
 
 # Specify your Sonic Screwdriver CAL (Catalog of Audio Listings) folder here:
 CAL_DIR = ".cal_sonic_library"
