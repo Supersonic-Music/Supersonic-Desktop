e36dfde6be645ab7efb78c595294c95bce39d77b
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2023-11-01 19:20:54 +0000

    Add comments to default config

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
+# Specify your Sonic Screwdriver music folder here:
 MUSIC_DIR = "http://localhost:8083/music"
+
+# Specify your Sonic Screwdriver CAL (Catalog of Audio Listings) folder here:
 CAL_DIR = ".cal_sonic_library"
