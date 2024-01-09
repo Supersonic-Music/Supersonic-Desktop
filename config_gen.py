b5c3d0811a2b8684976ae8ff4e861b318b5b5af9
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2024-01-09 20:06:50 +0000

    working on new config system

---
+++ config_gen.py
@@ -0,0 +1,40 @@
+import os
+
+LINUX_CONFIG_PATHS = [
+    "~/.config/supersonic-desktop/config.py", 
+    "/etc/supersonic-desktop/config.py", 
+    ".local/share/supersonic-desktop/config.py", 
+    "/usr/local/share/supersonic-desktop/config.py", 
+    "/usr/share/supersonic-desktop/config.py"
+]
+
+WINDOWS_CONFIG_PATHS = [
+    "%APPDATA%/supersonic-desktop/config.py",
+    "%LOCALAPPDATA%/supersonic-desktop/config.py",
+    "%PROGRAMDATA%/supersonic-desktop/config.py"
+]
+
+GENERIC_CONFIG_PATHS = [
+    "config.py", 
+    "~/Library/Application Support/supersonic-desktop/config.py",
+    "/Library/Application Support/supersonic-desktop/config.py",
+    "/usr/local/share/supersonic-desktop/config.py"
+]
+
+def loop_paths(paths, os_name):
+    for path in WINDOWS_CONFIG_PATHS:
+        if os.path.exists(path):
+            return path
+        if os_name in ["posix", "nt"]:
+            raise OSError("OS not supported")
+        else:
+            raise FileNotFoundError("Config file could not found")
+
+def find_config():
+    os_name = os.name()
+    if os_name == "posix":
+        loop_paths(LINUX_CONFIG_PATHS)
+    elif os_name == "nt":
+        loop_paths(WINDOWS_CONFIG_PATHS)
+    else:
+        loop_paths(GENERIC_CONFIG_PATHS)
